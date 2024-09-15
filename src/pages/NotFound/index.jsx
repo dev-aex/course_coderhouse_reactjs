55b5f97a3444e9f6b9f04b4b3d1f6d2bf7c73744
AEX <139301734+AEXdes@users.noreply.github.com>
2024-09-15 17:46:34 -0400

    [FEA]: 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,11 +1,18 @@
 import Base from "../../layout/Base";
+import NavBar from "../../components/NavBar";
 
 const NotFound = () => {
   return (
     <Base>
-    <h1>NotFound</h1>
+      <NavBar />
+      <div className="w-full h-full mt-3xl flex justify-center ">
+        <p className="flex flex-col text-ferre_blue200">
+          <span className="text-[112px] text-center">404</span>
+          <span className="text-[32px] text-center">Página no encontrada</span>
+        </p>
+      </div>
     </Base>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
