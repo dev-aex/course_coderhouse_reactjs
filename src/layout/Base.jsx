f14998218fa9311cfd4bdc8f5f2a7b62967a8fe0
AEX <139301734+AEXdes@users.noreply.github.com>
2024-09-28 18:05:10 -0400

    [DOC]

--- src/layout/Base.jsx
+++ src/layout/Base.jsx
@@ -1,12 +1,12 @@
 // Components
-import TopBar from "../components/TopBar";
+import Topbar from "../components/Topbar";
 import FooterBar from "../components/FooterBar";
 
 const Base = ({ children }) => {
   return (
     <div className="w-screen h-screen flex flex-col">
       <header className="mt-[8.9rem] mx-[13.8rem] flex flex-col gap-4xl">
-        <TopBar />
+        <Topbar />
       </header>
       <main className="flex-grow mt-3xl mx-[13.8rem] flex flex-col gap-4xl">
         {children}
