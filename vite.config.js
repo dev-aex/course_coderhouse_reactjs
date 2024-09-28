e24a7d22d31523b6dfa637b2a9dc7a06afbe9fbe
AEX <139301734+AEXdes@users.noreply.github.com>
2024-09-28 17:51:43 -0400

    [DOC]

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/course_coderhouse_reactjs",
+  base: "./",
   plugins: [react()],
 });
